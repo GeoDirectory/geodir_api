d741a306f57dce06e0403ca08219a50c82e0a6c4
Kiran Prajapati <kprajapati@thakkertech.com>
2017-06-06 13:17:08 +0530

    Allow listing results to filter by latitude & longitude - CHANGED

--- includes/geodir-rest-functions.php
+++ includes/geodir-rest-functions.php
@@ -565,13 +565,13 @@ function geodir_rest_validate_value_from_schema( $value, $args, $param = '' ) {
     if ( ! empty( $args['enum'] ) ) {
         if ( is_array( $value ) ) {
             foreach ( $value as $index => $v ) {
-                if ( ! in_array( $v, $args['enum'], true ) ) {
+                if ( ! in_array( $v, $args['enum'] ) ) {
                     /* translators: 1: parameter, 2: value, 3: list of valid values */
                     return new WP_Error( 'rest_invalid_param', sprintf( __( '%1$s value "%2$s" is not one of %3$s.' ), $param, $v, implode( ', ', $args['enum'] ) ) );
                 }
             }
         } else {
-            if ( ! in_array( $value, $args['enum'], true ) ) {
+            if ( ! in_array( $value, $args['enum'] ) ) {
                 /* translators: 1: parameter, 2: list of valid values */
                 return new WP_Error( 'rest_invalid_param', sprintf( __( '%1$s is not one of %2$s.' ), $param, implode( ', ', $args['enum'] ) ) );
             }
